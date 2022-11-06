bd97fb252720bb4dc1355dabebbde8a21538af01
Deborah Elsie Muthiani <DMuthiani@safaricom.co.ke>
2022-11-06 15:39:29 +0300

    used the else if functions to get the results for the answers

--- js/quizboard.js
+++ js/quizboard.js
@@ -1,1 +1,17 @@
 var answers = ["before body end", "script", "script", "isInteger", "Increased interactivity", "forEach"]
+$(document).ready(function() {
+$(".quiz").submit(function(event) {
+		var nameInput = $("input#name").val();
+		var emailInput = $("input#email").val();
+		var dob = $("#date").val();
+		var question1 = $("input:radio[name=question1]:checked").val();
+		var question2 = $("input:radio[name=question2]:checked").val();
+		var question3 = $("input:radio[name=question3]:checked").val();
+		var question4 = $("input:radio[name=question4]:checked").val();
+		var question5 = $("input:radio[name=question5]:checked").val();
+		var question6 = $("input:radio[name=question6]:checked").val();
+		 $("input").prop("disabled", true);
+         if (question1 === undefined || question2 === undefined || question3 === undefined || question4 === undefined || question5 === undefined ||question6 === undefined ||question7 === undefined ) {
+			alert("Please answer all questions")
+		}
+        
