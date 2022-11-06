544a6a7fd8c9b58b4adc707b4851fcf240c3df42
Deborah Elsie Muthiani <DMuthiani@safaricom.co.ke>
2022-11-06 15:45:37 +0300

    labelled the buttons for the quiz questions

--- js/quizboard.js
+++ js/quizboard.js
@@ -14,4 +14,58 @@ $(".quiz").submit(function(event) {
          if (question1 === undefined || question2 === undefined || question3 === undefined || question4 === undefined || question5 === undefined ||question6 === undefined ||question7 === undefined ) {
 			alert("Please answer all questions")
 		}
-        
+        function results() {
+			return nameInput + " you got " + correct;
+		}
+
+		var correct = 0
+		if (question1 == answers[0]) {
+			correct += 16.67;
+		} else if (question1 != answers[0]) {
+			correct += 0;
+		}
+		if (question2 == answers[1]) {
+			correct += 16.67;
+		} else if (question2 != answers[1]) {
+			correct += 0;
+		}
+		if (question3 == answers[2]) {
+			correct += 16.66;
+		} else if (question3 != answers[2]) {
+			correct += 0;
+		}
+		if (question4 == answers[3]) {
+			correct += 16.67;
+		} else if (question4 != answers[3]) {
+			correct += 0;
+		}
+		if (question5 == answers[4]) {
+			correct += 16.67;
+		} else if (question5 != answers[4]) {
+			correct += 0;
+		}
+		if (question6 == answers[5]) {
+			correct += 16.66;
+		} else if (question6 != answers[5]) {
+			correct += 0;
+		}
+		var messages = ["Awesome!", "Fairly passed", "It is important you repeat this test"]
+		var photo = ["images/awesome.jpeg", "images/good.jpeg", "images/fail.jpeg", ]
+		var score;
+		if (correct >= 83.33) {
+			score = 0;
+		} else if (correct >= 49 && correct < 83) {
+			score = 1;
+		} else if (correct <= 33.2 && correct < 49) {
+			score = 2;
+		}
+		document.getElementById("messages").innerHTML = messages[score];
+		document.getElementById("results").innerHTML = results();
+		document.getElementById("photo").src = photo[score];
+
+		function myFunction() {
+			document.getElementById("retake").style.visibilty = "visible";
+		}
+		event.preventDefault();
+	});
+});
